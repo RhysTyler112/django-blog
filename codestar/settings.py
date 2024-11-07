edab94cbb022bab14bd738dc605957f2f411edfb
Rhys Tyler <rhystyler.cardiff@gmail.com>
2024-11-07 19:50:21 +0000

    update DEBUG to False

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-rhystyler112-djangoblog-mu4jzovnhb9.ws.codeinstitute-ide.net', '.herokuapp.com']
 
